48e136e87883c6332450bd8b095eac3a48281973
Yeongjae Kim <sees77770@gmail.com>
2024-11-25 21:31:52 +0900

    Initial commit

---
+++ src/main/java/com/example/java_persistence_api/JavaPersistenceApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_persistence_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPersistenceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPersistenceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/java_persistence_api/JavaPersistenceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_persistence_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPersistenceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
