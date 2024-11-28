c3248a1cbde25121d99f135efa82a920c146a6a8
Yeongjae Kim <sees77770@gmail.com>
2024-11-28 19:56:13 +0900

    Key generate strategy

--- src/main/java/mapping/entity/Board.java
+++ src/main/java/mapping/entity/Board.java
@@ -4,12 +4,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.TableGenerator;
 
 @Entity
+@TableGenerator(
+        name = "BOARD_SEQ_GENERATOR",
+        table = "MY_SEQUENCES",
+        pkColumnValue = "BOARD_SEQ", allocationSize = 1)
 public class Board {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.TABLE,
+            generator = "BOARD_SEQ_GENERATOR")
     private Long id;
 
     public Long getId() {
